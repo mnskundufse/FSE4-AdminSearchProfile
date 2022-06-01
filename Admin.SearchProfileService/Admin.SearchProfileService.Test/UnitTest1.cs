119c195a34ddd217c0ac6e3feba56679cf36e288
mnskundu <mnskundu.fse@gmail.com>
2022-06-01 20:09:41 +0530

    Initial Checkin for Test Project

---
+++ Admin.SearchProfileService/Admin.SearchProfileService.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Admin.SearchProfileService.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
